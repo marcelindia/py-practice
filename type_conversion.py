53680bf588f30262b91fb7d756082f252c89add6
Diana Marcelin <marcelindiana@yahoo.com>
2022-05-02 14:31:36 -0400

    practice with type and conversions

--- type_conversion.py
+++ type_conversion.py
@@ -1,7 +1,7 @@
 # using int
-# birth_year = input("Birth year: ")
-# age = 2019 - int(birth_year)
-# print(age)
+birth_year = input("Birth year: ")
+age = 2019 - int(birth_year)
+print(age)
 
 # using type
 birth_year = input("Birth year: ")
@@ -9,3 +9,8 @@ print(type(birth_year))
 age = 2019 - int(birth_year)
 print(type(age))
 print(age)
+
+# converting weight
+weight = input("What is your weight? ")
+pounds_to_kilo=.45 * int(weight)
+print(pounds_to_kilo)

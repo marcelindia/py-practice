71943998053dc5acbb07191aa51616f89cbe1640
Diana Marcelin <marcelindiana@yahoo.com>
2022-05-02 14:26:18 -0400

    pratice with variables

---
+++ type_conversion.py
@@ -0,0 +1,11 @@
+# using int
+# birth_year = input("Birth year: ")
+# age = 2019 - int(birth_year)
+# print(age)
+
+# using type
+birth_year = input("Birth year: ")
+print(type(birth_year))
+age = 2019 - int(birth_year)
+print(type(age))
+print(age)

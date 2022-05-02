b5f4a518aedd7f99c3dba6a9cdf47ae35412c320
Diana Marcelin <marcelindiana@yahoo.com>
2022-05-02 14:14:06 -0400

    pratice with variables

---
+++ practice.py
@@ -0,0 +1,11 @@
+# name1 = "Debbie"
+# name2 = "Alice"
+# favorite_color= "yellow"
+# question= input("What is your name? ")
+# print(name1 + " likes " + favorite_color)
+
+
+# Quicker way to execute
+name = input('What is your name? ')
+favorite_color= input("What is your favorite color? ")
+print(name + " likes " + favorite_color)
